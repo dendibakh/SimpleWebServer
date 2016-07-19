21b3c4cc084e17f57739c6f0b79bd6521bf8497e
Denis Bakhvalov <denis.bakhvalov@nokia.com>
2016-07-19 09:55:13 +0200

    Check that server accepting connections asynchronously

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,9 @@
 #include "server.hpp"
 #include <boost/filesystem.hpp>
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <future>
 
 #include "http_parser.h"
 
@@ -13,6 +16,7 @@ public:
 	HttpServer(const ip::tcp::endpoint& ep, std::string homeDir);
 	void start();
 	void doAsyncAccept();
+	void doAsyncRead();
 
 	HttpServer(const HttpServer& rhs) = delete;
 	HttpServer& operator=(const HttpServer& rhs) = delete;
@@ -38,8 +42,9 @@ void HttpServer::start()
 
 void HttpServer::doAsyncAccept()
 {
+	std::cout << "Accepting new connection.\n";
 	acceptor.async_accept(socket,
-		   [&](boost::system::error_code ec)
+		   [this](boost::system::error_code ec)
 		   {
 			 // Check whether the server was stopped by a signal before this
 			 // completion handler had a chance to run.
@@ -52,6 +57,8 @@ void HttpServer::doAsyncAccept()
 			 {
 				 std::cout << "New connection established.\n";
 				 ip::tcp::socket newSock = std::move(socket);
+				 std::thread th1([this](){doAsyncRead();});
+				 th1.detach();
 				 //connection_manager_.start(std::make_shared<connection>(
 				   //std::move(socket), connection_manager_, request_handler_));
 			 }
@@ -59,6 +66,13 @@ void HttpServer::doAsyncAccept()
 			 // spawn a new async acception
 			 doAsyncAccept();
 		   });
+}
+
+void HttpServer::doAsyncRead()
+{
+	std::cout << "    Start request handling.\n";
+	std::this_thread::sleep_for (std::chrono::seconds(20));
+	std::cout << "    Finished request handling.\n";
 }
 
 void startServer(const boost::asio::ip::address addr, int port, const std::string& homeDir)
